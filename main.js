45a97f5c2a999fa7c77028d90109ea95677cf01c
Cristhina <viszellechacon@gmail.com>
2025-05-26 01:08:49 -0500

    update

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ class Game {
     this.player = new Player(this);
     this.sound = new AudioControl();
     this.obstacles = [];
-    this.numberOfObstacles = 10;
+    this.numberOfObstacles = 50;
     this.gravity;
     this.speed;
     this.minSpeed;
